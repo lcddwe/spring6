c4093d989cd9a0531ee268da53da7c6d1efe3b29
林聪 <2635128843@qq.com>
2022-11-09 16:52:21 +0800

    util命名空间的复用设置测试

--- spring6-003-dependency-injection/src/test/java/com/chunglam/spring6/test/SpringTest.java
+++ spring6-003-dependency-injection/src/test/java/com/chunglam/spring6/test/SpringTest.java
@@ -3,6 +3,7 @@ package com.chunglam.spring6.test;
 
 import com.chunglam.spring6.bean.SimpleValueType;
 import com.chunglam.spring6.bean.User;
+import com.chunglam.spring6.util.JdbcDataSource;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -29,5 +30,15 @@ public class SpringTest {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         SimpleValueType simpleType = applicationContext.getBean("simpleType", SimpleValueType.class);
         System.out.println(simpleType);
+    }
+
+    /**
+     * 测试util命名空间的复用
+     * */
+    @Test
+    public void utilTest() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        JdbcDataSource jdbcDataSource = applicationContext.getBean("myDataSource", JdbcDataSource.class);
+        System.out.println(jdbcDataSource);
     }
 }
